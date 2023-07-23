905deb7be7054938c773982f42947041f4826007
Oplala <oplala2020@gmail.com>
2023-07-23 20:57:24 +0530

    changes

--- server/db.js
+++ server/db.js
@@ -1,13 +1,4 @@
-// // import mongoose from 'mongoose';
-// const mongoose = require('mongoose');
-// const mongoURI = 'mongodb://oplala2020:q3BOK855holswJc8@ac-veculrv-shard-00-00.tlzwitv.mongodb.net:27017,ac-veculrv-shard-00-01.tlzwitv.mongodb.net:27017,ac-veculrv-shard-00-02.tlzwitv.mongodb.net:27017/?ssl=true&replicaSet=atlas-ubwzhf-shard-0&authSource=admin&retryWrites=true&w=majority'
-// const connectToMongo = () => {
-//     mongoose.connect(mongoURI, () => {
-//         console.log("connected succesfully")
-//     })
-// }
 
-// module.exports = connectToMongo
 import mongoose from 'mongoose';
 
 
--- src/component/Noteitem.js
+++ src/component/Noteitem.js
@@ -8,7 +8,7 @@ const Noteitem = (props) => {
             <div className="card my-3">
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
-                    <p className="card-text">{note.description} Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum necessitatibus ipsa placeat sit officiis quod iste vel minima maiores eligendi nihil porro, fugit possimus. Ut alias rem asperiores, modi maxime obcaecati delectus eos error inventore aliquam doloremque cumque accusamus explicabo ipsum velit commodi facere, dignissimos quibusdam quod. Commodi, magnam illo!</p>
+                    <p className="card-text">{note.description} Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum necessitatibus ipsa placeat sit officiis quod iste vel minima maiores eligendi nihil porro, fugit possimus. Ut alias rem asperiores, modi maxime obcaecati delectus eos error inventore aliquam doloremque cumque accusamus explicabo ipsum velit commodi facere, dignissimos quibusdam quod. Commodi, magnam illo</p>
                 </div>
             </div>
         </div>
